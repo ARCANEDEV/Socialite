ac597e123491e5e72c6abee8f10b38c3583d0421
ARCANEDEV <arcanedev.maroc@gmail.com>
2016-01-19 13:53:52 +0000

    Adding LinkedIn Provider

---
+++ src/OAuth/Two/LinkedInProvider.php
@@ -0,0 +1,112 @@
+<?php namespace Arcanedev\Socialite\OAuth\Two;
+
+use Arcanedev\Socialite\Base\OAuthTwoProvider;
+
+/**
+ * Class     LinkedInProvider
+ *
+ * @package  Arcanedev\Socialite\OAuth\Two
+ * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+class LinkedInProvider extends OAuthTwoProvider
+{
+    /* ------------------------------------------------------------------------------------------------
+     |  Properties
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * The scopes being requested.
+     *
+     * @var array
+     */
+    protected $scopes = ['r_basicprofile', 'r_emailaddress'];
+
+    /**
+     * The fields that are included in the profile.
+     *
+     * @var array
+     */
+    protected $fields = [
+        'id', 'first-name', 'last-name', 'formatted-name',
+        'email-address', 'headline', 'location', 'industry',
+        'public-profile-url', 'picture-url', 'picture-urls::(original)',
+    ];
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Getters & Setters
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * {@inheritdoc}
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase('https://www.linkedin.com/uas/oauth2/authorization', $state);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenUrl()
+    {
+        return 'https://www.linkedin.com/uas/oauth2/accessToken';
+    }
+
+    /**
+     * Get the POST fields for the token request.
+     *
+     * @param  string  $code
+     *
+     * @return array
+     */
+    protected function getTokenFields($code)
+    {
+        return parent::getTokenFields($code) + ['grant_type' => 'authorization_code'];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getUserByToken($token)
+    {
+        $fields   = implode(',', $this->fields);
+        $url      = 'https://api.linkedin.com/v1/people/~:('.$fields.')';
+        $response = $this->getHttpClient()->get($url, [
+            'headers' => [
+                'x-li-format' => 'json',
+                'Authorization' => 'Bearer '.$token,
+            ],
+        ]);
+
+        return json_decode($response->getBody(), true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User)->setRaw($user)->map([
+            'id'              => $user['id'],
+            'nickname'        => null,
+            'name'            => array_get($user, 'formattedName'),
+            'email'           => array_get($user, 'emailAddress'),
+            'avatar'          => array_get($user, 'pictureUrl'),
+            'avatar_original' => array_get($user, 'pictureUrls.values.0'),
+        ]);
+    }
+
+    /**
+     * Set the user fields to request from LinkedIn.
+     *
+     * @param  array  $fields
+     *
+     * @return self
+     */
+    public function fields(array $fields)
+    {
+        $this->fields = $fields;
+
+        return $this;
+    }
+}
