047e50b7685ecedfe3b20cb39fb75e977b63254c
ARCANEDEV <arcanedev.maroc@gmail.com>
2016-01-19 13:54:52 +0000

    Adding Bitbucket provider

---
+++ src/OAuth/One/BitbucketProvider.php
@@ -0,0 +1,14 @@
+<?php namespace Arcanedev\Socialite\OAuth\One;
+
+use Arcanedev\Socialite\Base\OAuthOneProvider;
+
+/**
+ * Class     BitbucketProvider
+ *
+ * @package  Arcanedev\Socialite\OAuth\One
+ * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+class BitbucketProvider extends OAuthOneProvider
+{
+    //
+}
